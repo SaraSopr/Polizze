ddf311b90411572228979fc596dfdbfda1ee09e6
giuseppe.salvio <giuseppe.salvio@arcavita.it>
2024-03-20 17:14:38 +0100

    test di main application per coverage al 100%

---
+++ src/test/java/com/fincons/PolizzeApplicationTest.java
@@ -0,0 +1,15 @@
+package com.fincons;
+
+import com.fincons.db.entity.PolizzaDb;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolizzeApplicationTest {
+
+    @Test
+    void main() {
+        PolizzeApplication.main(new String[]{});
+    }
+}
